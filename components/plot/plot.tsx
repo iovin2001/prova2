7278470dcee3ff0ecc00baa82821c86fcf8f27a7
iovin2001 <100416507+iovin2001@users.noreply.github.com>
2024-04-27 00:32:52 +0200

    Update plot.tsx

--- components/plot/plot.tsx
+++ components/plot/plot.tsx
@@ -46,7 +46,7 @@ const StatsGroup: React.FC = () => {
     { title: 'Total Staked', value: '0', Icon: <GroupIcon />, bgColor: '#33f4c8' },
     { title: 'TVL (USD)', value: '$0', Icon: <AccountBalanceIcon />, bgColor: '#33c7c8' },
     { title: '$ASS Distributed', value: '0', Icon: <MonetizationOnIcon />, bgColor: '#1dcdfe' },
-
+    { title: 'SYAKU Daily Yield', value: '4', Icon: <ShowChartIcon />, bgColor: '#18b1db' },
   ];
 
   return (

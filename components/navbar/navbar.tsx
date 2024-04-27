aec0d77a69996270363086010d5c5e2418d68c54
iovin2001 <100416507+iovin2001@users.noreply.github.com>
2024-04-27 02:16:17 +0200

    Update navbar.tsx

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -239,7 +239,7 @@ const NavigationMenuDemo: React.FC<NavigationMenuDemoProps> = ({ isPlace, onCrea
             isPlace ? (
     <div className="flex space-x-2">
       <NavigationMenu.Item className='p-1 flex items-center' key="migrate-button-1">
-        <Image src="./discord-icon-svgrepo-com.svg" alt="Discord" width={40} height={40} />
+        <Image src="./1.svg" alt="Discord" width={40} height={40} />
 
         <ConnectButton connectText="Discord" style={{ color: "#000000" }} />
       </NavigationMenu.Item>
